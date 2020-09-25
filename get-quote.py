3afc460088ffbf139cf9508ee073212dd4afa77a
debasis6655 <71864892+debasis6655@users.noreply.github.com>
2020-09-25 11:08:41 +0530

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,10 +1,12 @@
-def primery():
+def main():
     print("Keep it logically awesome.")
 
     f = open("quotes.txt")
     quotes = f.readlines()
     f.close()
+    
     print(quotes[0])
+    
 #if __name__== "__main__":
   #main()
 
